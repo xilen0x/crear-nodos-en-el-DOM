0b1bad3f386e01dd86b48dcde61c2c7c6eb3f502
Carlos Astorga <xilenox@localhost.localdomain>
2020-05-25 19:26:58 -0400

    ejercicio2

--- app.js
+++ app.js
@@ -7,6 +7,8 @@
 //5.AGREGAR EL ELEMENTO AL DOCUMENTO
 /////////////**********************************///////////////////// */
 
+/**ejericio 1 */
+
 //1.CREAR EL ELEMENTO
 var elemento = document.createElement("h2");
 //2.CREAR UN NODO DE TEXTO
@@ -17,3 +19,28 @@ elemento.appendChild(contenido);
 elemento.setAttribute("align", "center");
 //5.AGREGAR EL ELEMENTO AL DOCUMENTO
 document.getElementById('root').appendChild(elemento);
+
+/* Fin ejercicio 1 */
+/**Ejericio 2 */
+
+//1.CREAR EL ELEMENTO
+var elemento = document.createElement("li");
+
+//2.CREAR UN NODO DE TEXTO
+var contenido = document.createTextNode("Texto de la lista agregado");
+
+//3.AÑADIR EL NODO DE TEXTO AL ELEMENTO
+elemento.appendChild(contenido);
+
+//5.AGREGAR EL ELEMENTO AL DOCUMENTO (el nuevo li se agregará al final.)
+//  document.getElementById('lista').appendChild(elemento);
+
+    //Otra manera de hacerlo sería:
+  var padre = document.getElementsByTagName("li")[0].parentNode;
+  padre.appendChild(elemento);
+
+//Para agtegar el elemento al inicio de la lista:
+var hijo = document.getElementsByTagName("li")[0];
+padre.insertBefore(elemento, hijo);
+
+/* Fin ejercicio 2 */
